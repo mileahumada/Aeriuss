51b3ba7fcffcc89115c51e3042a0904405a29a4a
lucas maisonnave <est.maisonnave.lucas@latecnicalf.com.ar>
2026-03-19 19:44:01 -0300

    hice que funcione el inicio de sesion

--- formularios.php
+++ formularios.php
@@ -1,23 +1,42 @@
 <?php
-  // Usuario y contraseña predefinidos
-  $usuario_valido = "admin@gmail.com";
-  $clave_valida = "12345678";
+include 'conexion.php';
+session_start();
 
-  // Si el formulario ha sido enviado
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $emails = $_POST['email'];
-    $clave = $_POST['contraseña'];
+$usuario = $_POST['email'];
+$clave = $_POST['contraseña'];
 
-    // Comparar con los valores permitidos
-    if ($emails === $usuario_valido && $clave === $clave_valida) {
-  header('Location: inicio.html');
-    } 
-  else
-  {
-    echo "<script>
-          alert('email o/y contraseña incorrecto');
-          window.location.href = 'inicioSesion.html';
-        </script>";
-  exit();
-  }
+/*En PHP, $stmt hace referencia a un objeto de declaración preparada (Prepared Statement) cuando trabajas con bases de datos mediante MySQLi o PDO. Se usa principalmente para ejecutar consultas SQL de manera más segura y eficiente, especialmente cuando se manejan entradas de usuarios, ya que ayuda a prevenir ataques de inyección SQL.*/
+// Preparar la consulta
+$stmt = $conexion->prepare("SELECT contraseña FROM usuarios WHERE email = ?");
+
+// Vincular el parámetro (usuario)
+$stmt->bind_param("s", $usuario);
+
+// Ejecutar la consulta
+$stmt->execute();
+
+// Va a guardar el resultado de la busqueda en la DB en $password_hash_db
+$stmt->bind_result($password_hash_db);
+
+// Obtener el resultado, La función $stmt->fetch() en PHP se utiliza para recuperar el siguiente resultado de una consulta ejecutada con una declaración preparada (Prepared Statement). Después de vincular variables con bind_result(), fetch() asigna los valores de la fila actual del conjunto de resultados a esas variables.
+
+
+if ($stmt->fetch()) {
+    // password_verify() toma la contraseña ingresada y el hash almacenado y verifica si el hash de la contraseña ingresada coincide con el hash almacenado.
+    if (password_verify($clave, $password_hash_db)) {
+        header("Location: inicio.php");
+        $_SESSION["usuario"] = $usuario;
+        exit; // detenemos el script
+    } else {
+        echo "Contraseña incorrecta";
+    }
+} else {
+    echo "<script type='text/javascript'>alert('Usuario No Encontrado');
+        window.location.href = 'iniciophp';</script>";
 }
+
+// Cerrar la declaración
+$stmt->close();
+//cerramos la conexion a la base de datos
+$conexion->close();
+?>
